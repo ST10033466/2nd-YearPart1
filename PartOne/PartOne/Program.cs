0b39fe860b9653bb561fbf95fab6e56893d0193f
Isimael <103181281+ST10033466@users.noreply.github.com>
2023-04-14 10:41:52 +0200

    Add files via upload

---
+++ PartOne/PartOne/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartOne
+{
+    class Global
+    {
+        public static string recipe, displayRecipe, scaledRecipe, reset, clear, exitProgram;
+
+        public static int mainMenuOption = 0;
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //A welcome message, welcoming users
+            Console.WriteLine("Welcome to Recipe World\n");
+
+            Menu();
+
+
+
+
+        }
+            public static void Menu()
+        {
+            //A do while loop create so the program can run untill the user exits the program
+            do
+            {
+                //A  main menu for the console app, promting the user to input an option.
+                Console.WriteLine("Select an option\n" +
+                    "1. Enter a recipe\n" +
+                    "2. Display the full recipe\n" +
+                    "3. Change the quantity scaled\n" +
+                    "4. Reset the quantity scaled\n" +
+                    "5. Clear all data\n" +
+                    "6. Exit program");
+                Global.mainMenuOption = Convert.ToInt32(Console.ReadLine());
+
+                Recipe myRecipe = new Recipe();
+
+           //     int len= myRecipe.Name.Length;
+
+                //If statement create to perform the actions of selected from the main menu option
+                if (Global.mainMenuOption == 1)
+                {
+
+                    //Calling the obect of the recipe class so it can perform the functions of the class.
+                    Recipe.ingredients();
+
+                }
+                else if (Global.mainMenuOption == 2)
+                {
+                    Recipe.DisplayMenu();
+
+                    Recipe.ingredients();
+
+                    //if (len.Equals(0))
+                    //{
+                    //    Console.WriteLine("You have not entered a recipe");
+
+                    //}
+                    //else { Recipe.DisplayMenu(); }
+
+
+                }
+                else if (Global.mainMenuOption == 3)
+                {
+
+                }
+                else if (Global.mainMenuOption == 4)
+                {
+
+                }
+                else if (Global.mainMenuOption == 5)
+                {
+
+                }
+                else if (Global.mainMenuOption == 6)
+                {
+
+                }
+
+
+            } while (Global.mainMenuOption > 6);
+
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ PartOne/PartOne/Recipe.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartOne
+{
+    internal class Recipe
+    {
+        class Global {
+
+            public static int numOfIngri;
+            public static int numOfSteps;
+            
+        }
+
+        private static string[] nameIngri;
+        private static string[] unitOfMeasure;
+        private static string[] descripSteps;
+
+        public static int[] Quantity { get; set; }
+
+        public string[] Name { 
+            get { return nameIngri; } 
+            set { nameIngri = value; } }
+
+        public Recipe(string[] nameIngri, string[] unitOfMeasure, string[] descripSteps)
+        {
+            Name = nameIngri;
+            Unit = unitOfMeasure;
+            Description = descripSteps;
+        }
+
+        public Recipe()
+        {
+        }
+
+        public string[] Unit {
+            get { return unitOfMeasure; }
+            set { unitOfMeasure = value; } }
+            
+
+        public string[] Description {
+            get { return descripSteps; } 
+            set { descripSteps = value; } }
+
+
+
+
+        public static void ingredients()
+        {
+            Console.WriteLine("How many Ingridents would you like to add?");
+            Global.numOfIngri = Convert.ToInt32(Console.ReadLine());
+
+
+            Quantity = new int[Global.numOfIngri];
+            nameIngri = new string[Global.numOfIngri];
+            unitOfMeasure = new string[Global.numOfIngri];
+            
+
+            for (int i = 0; Global.numOfIngri > i; i++)
+            {
+
+                Console.WriteLine("What is the name of ingridents " + (i + 1) + " ?");
+                nameIngri[i] = Console.ReadLine();
+
+
+
+                Console.WriteLine("What is the qunatity of ingridents " + (i + 1) + " ?");
+                Quantity[i] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What is the unit of measurement of ingridents " + (i + 1) + " ?");
+                unitOfMeasure[i] = Console.ReadLine();
+
+
+
+            }
+
+            Console.WriteLine("How many steps are there? ");
+            Global.numOfSteps = Convert.ToInt32(Console.ReadLine());
+
+            descripSteps = new string[Global.numOfSteps];
+
+            for (int i = 0; Global.numOfSteps > i; i++)
+            {
+                Console.WriteLine("Step " + (i + 1));
+                descripSteps[i] = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("You have entered the details successfully\n");
+
+            Program.Menu();
+
+            Console.ReadKey();
+
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("Recipe: ");
+            Console.WriteLine("*************************\n");
+
+            Console.WriteLine("Ingredients");
+            Console.WriteLine("************************");
+            Console.WriteLine("Number of Ingredients: " + Global.numOfIngri);
+            for (int j = 0; Global.numOfIngri > j; j++)
+            {
+                
+                Console.WriteLine("Name of Ingredient: " + nameIngri[j]);
+                Console.WriteLine("Quantity of Ingredient: " + Quantity[j]);
+                Console.WriteLine("Unit of Measurement: "+ unitOfMeasure[j]);
+            }
+
+            Console.WriteLine("************************************");
+            Console.WriteLine("Step: "+ Global.numOfSteps);
+            for (int k = 0; Global.numOfSteps > k; k++)
+            {
+                Console.WriteLine("Step " + (k+1)+": "+ descripSteps[k]);
+            }
+            
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}

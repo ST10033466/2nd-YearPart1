ef821e9af840e5ebc19926ce2b6bbcbf7139375e
Isimael <103181281+ST10033466@users.noreply.github.com>
2023-04-26 17:24:06 +0200

    Update Ingredients.cs

--- PartOne/PartOne/Ingredients.cs
+++ PartOne/PartOne/Ingredients.cs
@@ -9,14 +9,14 @@ namespace PartOne
     internal class Ingredients
     {
 
-
+        // These static variables will store the data entered by the user.
         private static int numOfIngri, numOfSteps;
         private static int[] ingridentQuantity;
         private static string[] ingredientName;
         private static string[] unitOfMeasurement;
         private static string[] descriptionOfSteps;
 
-
+        // These properties allow external code to access and modify the static variables.
         public int[] Quantity 
         { get {return ingridentQuantity; }
             set {ingridentQuantity = value; } 
@@ -58,7 +58,7 @@ namespace PartOne
 
 
 
-
+        // This method prompts the user to input the ingredient details and stores them in the static variables.
         public static void InputtingIngredients()
         {
 
@@ -147,17 +147,20 @@ namespace PartOne
 
             Console.WriteLine("\nYou have entered the details successfully\n");
 
+            // Calls the MainMenu method in the Program class to go back to the main menu.
             Program.MainMenu();
 
             Console.ReadKey();
 
         }
 
+        // This method displays the ingredients and steps entered by the user.
         public static void DisplayingMenu()
         {
-
+            // creating an object of the ScaledQuantity class
             ScaledQuantity myobj = new ScaledQuantity();
 
+            // printing the header and ingredients section of the recipe
             Console.WriteLine("*********************************");
             Console.WriteLine("        Recipe Dispaly \n");    
             Console.WriteLine("Ingredients:");
@@ -168,12 +171,10 @@ namespace PartOne
 
                 Console.WriteLine("Name of Ingredient: " + ingredientName[j]);
                 Console.WriteLine("Quantity of Ingredient: " + ingridentQuantity[j]);
-               
-                //ScaledQuantity.resettingScaledQuantity();
                 Console.WriteLine("Unit of Measurement: " + unitOfMeasurement[j] + "\n");
             }
 
-            
+            // printing the steps section of the recipe
             Console.WriteLine("             Steps \n");
             for (int k = 0; numOfSteps > k; k++)
             {
@@ -185,22 +186,23 @@ namespace PartOne
 
         }
 
+        // This method clears the data from the recipe
         public static void ClearingData() 
         {
-            //This is the variable that stores the option for clearing data or not.
+            // variable that stores the user's confirmation to clear the data
             int confirmationOFClearing = 0;
 
+            // prompting user to confirm if they want to clear the data or not
             Console.Write("Would you like to clear the data: " +
                 "1. Yes \n" +
                 "2. No \n" +
                 "3. Go back to menu \n" +
                 "Enter your option: ");
 
-            //Try and catch used so that the program can pick up if the user made an incorrect input
-            //and re-prompt them.
-            try 
+            // using try-catch to handle incorrect user input
+            try
             {
-                //Stroing user input for the 
+                // storing user input for confirmation
                 confirmationOFClearing = Convert.ToInt32(Console.ReadLine());
             }
             catch (Exception e) 
@@ -208,9 +210,11 @@ namespace PartOne
                 Console.Write("Incorrect Input, Type in a number: ");
             }
 
-
-            switch(confirmationOFClearing)
+            // using a switch statement to handle the user's choice
+            switch (confirmationOFClearing)
             {
+
+                // if the user chooses to clear the data, all the recipe data is reset to null or 0
                 case 1:
 
                     numOfIngri = 0;
@@ -223,11 +227,15 @@ namespace PartOne
                     Console.WriteLine("You have cleared!");
 
                     break;
+
+                // if the user chooses not to clear the data, a message is printed indicating their choice
                 case 2:
 
                     Console.WriteLine("You have decided to not clear the data");
 
                     break;
+
+                // if the user chooses to go back to the menu, the DisplayingMenu() method is called
                 case 3:
 
                     Ingredients.DisplayingMenu();
